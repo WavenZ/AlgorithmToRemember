ae1d9f9fbf1bb4e9a3b954e250225bbdb57d5b09
WavenZ <33001625+WavenZ@users.noreply.github.com>
2019-09-21 15:34:56 +0800

    Update 23.Quick_Sort.cpp

--- C++/src/23.Quick_Sort.cpp
+++ C++/src/23.Quick_Sort.cpp
@@ -11,11 +11,25 @@ public:
 private:
 	void quick_sort(std::vector<int>& vec, int start, int end) {
 		if (start >= end) return;
-		int pos = patition(vec, start, end);
+		int pos = randomized_patition(vec, start, end);
 		quick_sort(vec, start, pos - 1);
 		quick_sort(vec, pos + 1, end);
 	}
 	int patition(std::vector<int>& vec, int start, int end) {
+		int pivot = vec[end];
+		int pos = start;
+		for (int i = start; i < end; ++i) {
+			if (vec[i] < pivot) {
+				std::swap(vec[pos++], vec[i]);
+			}
+		}
+		std::swap(vec[pos], vec[end]);
+		return pos;
+	}
+	int randomized_patition(std::vector<int>& vec, int start, int end) {
+		std::default_random_engine rand_engine;
+		std::uniform_int_distribution<unsigned> uniform_rand(start, end);
+		std::swap(vec[end], vec[uniform_rand(rand_engine)]);
 		int pivot = vec[end];
 		int pos = start;
 		for (int i = start; i < end; ++i) {
