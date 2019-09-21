d61a15bfa42dc29c8e01864794614170a1ae8092
WavenZ <33001625+WavenZ@users.noreply.github.com>
2019-09-21 15:11:59 +0800

    Create 23.Quick_Sort.cpp

---
+++ C++/src/23.Quick_Sort.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <random>
+
+template<class T>
+class QuickSort {
+public:
+	void sort(std::vector<T>& vec) {
+		quick_sort(vec, 0, vec.size() - 1);
+	}
+private:
+	void quick_sort(std::vector<int>& vec, int start, int end) {
+		if (start >= end) return;
+		int pos = patition(vec, start, end);
+		quick_sort(vec, start, pos - 1);
+		quick_sort(vec, pos + 1, end);
+	}
+	int patition(std::vector<int>& vec, int start, int end) {
+		int pivot = vec[end];
+		int pos = start;
+		for (int i = start; i < end; ++i) {
+			if (vec[i] < pivot) {
+				std::swap(vec[pos++], vec[i]);
+			}
+		}
+		std::swap(vec[pos], vec[end]);
+		return pos;
+	}
+};
+
+
+
+int main(void) {
+	std::vector<int> vec = { 3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 8, 9, 8 };
+	QuickSort<int> Q;
+	Q.sort(vec);
+	return 0;
+}
